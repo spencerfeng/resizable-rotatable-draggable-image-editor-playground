49992bcb84341b999a7fdbe5697b30a95f7504d7
Spencer Feng <spencer.feng@lexicondigital.com.au>
2020-07-25 17:02:39 +1000

    feat: remove the default create-react-app image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.scss'
 import Frame from './components/Frame'
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Frame width={400} height={200} rotateAngle={-45} center={{ x: 800, y: 300 }} />
       </header>
     </div>
